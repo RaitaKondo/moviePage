960efae17c88e5ff2a2cf401b3a390ce37438695
Raita Kondo <raycondo915@gmail.com>
2023-12-12 15:00:45 +0700

    API fetch done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=c7dbba92";
 
 const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovie("Bucky Larson");
+  }, []);
+
   return <h1>App</h1>;
 };
 
